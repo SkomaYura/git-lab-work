a127c4730727f5e56610230ea00e24cab4570907
Yurii Skomarovskyi <skomarovskyyra@gmail.com>
2026-05-01 09:56:06 +0300

    feat: додаємо функцію farewell()

--- src/main.py
+++ src/main.py
@@ -4,5 +4,10 @@ def greet(name: str) -> str:
     """Повертає вітальне повідомлення."""
     return f"Привіт, {name}!"
 
+def farewell(name: str) -> str:
+    """Повертає прощальне повідомлення."""
+    return f"До побачення, {name}!"
+
 if __name__ == "__main__":
     print(greet("Студент"))
+    print(farewell("Студент"))
